394fc81bb9badee53da448c0b24861d53e858cce
stefan panait <stefan.panait@gmail.com>
2019-02-23 23:58:34 +0000

    incremental

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ document.write('<style class="hideStuff" ' +
 
 
    // wait for injected body and jquery to be available
-   var waitForGuests = setInterval(function () {
+var waitForGuests = setInterval(function () {
     // if jquery not available do not clear interval
     if (typeof guestElements[i].nextElementSibling.firstChild.className === 'undefined') return
     // we should check for main element's existence here
